6c90144dac0acac5917f90c8f64a41df61fd9cfe
Geoffrey Flarity <geoff@geoffreys-mbp.lan>
2021-03-20 21:28:56 -0400

    add support for specifying video player and options for it (useful for cross platform)

--- picker.ts
+++ picker.ts
@@ -4,10 +4,15 @@ import {
 import { readKeypress, Keypress } from "https://deno.land/x/keypress@0.0.7/mod.ts";
 import { CHAR_NO_BREAK_SPACE }    from "https://deno.land/std@0.88.0/path/_constants.ts";
 
-
 // processStdEntry prompts the user to delete or keep the file/entry (or quit altogether).
-async function processStdEntry(entry: WalkEntry) {
-  const p = Deno.run({cmd: ["mplayer", entry.path], stdout: "null"})
+async function processStdEntry(entry: WalkEntry, videoPlayer: string, videoPlayerOptions: string | undefined) {
+
+  // some video players (mpv) need options to be usable by default
+  let cmd = [videoPlayer]
+  if (videoPlayerOptions != null) {
+    cmd = cmd.concat(videoPlayerOptions)
+  }
+  const p = Deno.run({cmd: cmd.concat(entry.path), stdout: "null"})
   await p.status()
   console.log("(D)elete/(K)eep/(Q)uit")
   
@@ -25,13 +30,13 @@ async function processStdEntry(entry: WalkEntry) {
 }
 
 // processSkipEntry let's us treat the first N entries as quickly skippable which is useful for resuming a run for whatever reason.
-async function processSkipEntry(entry: WalkEntry): Promise<boolean> {
+async function processSkipEntry(entry: WalkEntry, videoPlayer: string, videoPlayerOptions: string | undefined): Promise<boolean> {
   console.log(`Processing ${entry.path}`)
   console.log("(S)kip/Any Key To Process")
   let complete = false;
   for await (const keypress of readKeypress()) {
       if (keypress.key != "s") {
-        await processStdEntry(entry)
+        await processStdEntry(entry, videoPlayer, videoPlayerOptions)
         complete = true
       }
       break
@@ -40,14 +45,16 @@ async function processSkipEntry(entry: WalkEntry): Promise<boolean> {
 }
 
 async function main() {
+  const videoPlayer = Deno.env.get("VIDEO_PLAYER") || "mplayer";
+  const videoPlayerOptions = Deno.env.get("VIDEO_PLAYER_OPTIONS")
   const walker = walk(".", { match: [new RegExp("(mp4|mkv|mov)$")] })
   
   let skipComplete = false
   for await (const entry of walker) {
     if (!skipComplete) {
-      skipComplete = await processSkipEntry(entry)
+      skipComplete = await processSkipEntry(entry, videoPlayer, videoPlayerOptions)
     } else {
-      await processStdEntry(entry)
+      await processStdEntry(entry, videoPlayer, videoPlayerOptions)
     }
   }
 }
