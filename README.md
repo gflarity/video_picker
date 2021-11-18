b978ca7e60578f6afb5737778d776dcc642fafd7
Geoff Flarity <geoff.flarity@gmail.com>
2021-11-18 18:22:40 -0500

    Revert "run deno fmt"
    This reverts commit 55b07d0f2b2a1b0e41124e838777b0dc3935d076.

--- README.md
+++ README.md
@@ -1,25 +1,23 @@
 # Intro
 
-The following is a functional learning project meant to achieve the following
-goals:
-
-1. Create a working utility as a means to learn [Deno](https://deno.land).
-2. Solve the problem of going through a collection of videos and quickly decided
-   to keep or delete them.
+The following is a functional learning project meant to achieve the following goals:
+  1. Create a working utility as a means to learn [Deno](https://deno.land).
+  2. Solve the problem of going through a collection of videos and quickly decided to keep or delete them.
 
 Note: This works well for me, but **use at your own risk**.
 
 # Usage
 
-Install deno by follow instructions etc [here](https://deno.land).
+Install deno by follow instructions etc [here](https://deno.land). 
 
 ```bash
 deno compile --unstable --allow-run --allow-read --allow-write --allow-env picker.ts
 ```
 
-Move the picker executable somewhere into your path. Then run it in the
-directory that contains the videos you wish to sort through.
+Move the picker executable somewhere into your path. Then run it in the directory that contains the videos you wish to sort through.
 
 ```bash
 picker
 ```
+
+
--- picker.ts
+++ picker.ts
@@ -1,91 +1,69 @@
 import {
-  walk,
-  WalkEntry,
-  WalkOptions,
-} from "https://deno.land/std@0.88.0/fs/mod.ts";
-import {
-  Keypress,
-  readKeypress,
-} from "https://deno.land/x/keypress@0.0.7/mod.ts";
-import { CHAR_NO_BREAK_SPACE } from "https://deno.land/std@0.88.0/path/_constants.ts";
+      walk, WalkEntry, WalkOptions
+    }                             from "https://deno.land/std@0.88.0/fs/mod.ts";
+import { readKeypress, Keypress } from "https://deno.land/x/keypress@0.0.7/mod.ts";
+import { CHAR_NO_BREAK_SPACE }    from "https://deno.land/std@0.88.0/path/_constants.ts";
 
 // processStdEntry prompts the user to delete or keep the file/entry (or quit altogether).
-async function processStdEntry(
-  entry: WalkEntry,
-  videoPlayer: string,
-  videoPlayerOptions: string | undefined,
-) {
-  console.log(`Processing std entry ${entry.path}`);
+async function processStdEntry(entry: WalkEntry, videoPlayer: string, videoPlayerOptions: string | undefined) {
+  console.log(`Processing std entry ${entry.path}`)
   // some video players (mpv) need options to be usable by default
-  let cmd = [videoPlayer];
+  let cmd = [videoPlayer]
   if (videoPlayerOptions != null) {
-    cmd = cmd.concat(videoPlayerOptions);
+    cmd = cmd.concat(videoPlayerOptions)
   }
-  const p = Deno.run({
-    cmd: cmd.concat(entry.path),
-    stdout: "null",
-    stderr: "null",
-  });
-  await p.status();
-  console.log("(D)elete/(K)eep/(R)eplay/(Q)uit");
-
-  FOR:
-  for await (const keypress of readKeypress()) {
+  const p = Deno.run({cmd: cmd.concat(entry.path), stdout: "null", stderr: "null"})
+  await p.status()
+  console.log("(D)elete/(K)eep/(R)eplay/(Q)uit")
+  
+  FOR: for await (const keypress of readKeypress()) {
     if (keypress.key == "q") {
-      Deno.exit(0);
+      Deno.exit(0)
     } else if (keypress.key == "d") {
-      console.log(`deleting ${entry.path}`);
-      await Deno.remove(entry.path);
+      console.log(`deleting ${entry.path}`)
+      await Deno.remove(entry.path)
     } else if (keypress.key == "r") {
-      console.log(`replaying ${entry.path}`);
-      await processStdEntry(entry, videoPlayer, videoPlayerOptions);
+      console.log(`replaying ${entry.path}`)
+      await processStdEntry(entry, videoPlayer, videoPlayerOptions)
     } else {
-      console.log(`Keeping ${entry.path}`);
+      console.log(`Keeping ${entry.path}`)
     }
-    break FOR;
+    break FOR
   }
 }
 
 // processSkipEntry let's us treat the first N entries as quickly skippable which is useful for resuming a run for whatever reason.
-async function processSkipEntry(
-  entry: WalkEntry,
-  videoPlayer: string,
-  videoPlayerOptions: string | undefined,
-): Promise<boolean> {
-  console.log(`Processing skip entry ${entry.path}`);
-  console.log("(S)kip/Any Key To Process");
+async function processSkipEntry(entry: WalkEntry, videoPlayer: string, videoPlayerOptions: string | undefined): Promise<boolean> {
+  console.log(`Processing skip entry ${entry.path}`)
+  console.log("(S)kip/Any Key To Process")
   let complete = false;
   for await (const keypress of readKeypress()) {
-    if (keypress.key != "s") {
-      await processStdEntry(entry, videoPlayer, videoPlayerOptions);
-      complete = true;
-    }
-    break;
+      if (keypress.key != "s") {
+        await processStdEntry(entry, videoPlayer, videoPlayerOptions)
+        complete = true
+      }
+      break
   }
-  return complete;
+  return complete
 }
 
 async function main() {
   const videoPlayer = Deno.env.get("VIDEO_PLAYER") || "mplayer";
-  const videoPlayerOptions = Deno.env.get("VIDEO_PLAYER_OPTIONS");
-  const walker = walk(".", { match: [new RegExp("(mp4|mkv|mov)$")] });
-
-  let skipComplete = false;
+  const videoPlayerOptions = Deno.env.get("VIDEO_PLAYER_OPTIONS")
+  const walker = walk(".", { match: [new RegExp("(mp4|mkv|mov)$")] })
+  
+  let skipComplete = false
   for await (const entry of walker) {
     if (!skipComplete) {
-      skipComplete = await processSkipEntry(
-        entry,
-        videoPlayer,
-        videoPlayerOptions,
-      );
+      skipComplete = await processSkipEntry(entry, videoPlayer, videoPlayerOptions)
     } else {
-      await processStdEntry(entry, videoPlayer, videoPlayerOptions);
+      await processStdEntry(entry, videoPlayer, videoPlayerOptions)
     }
   }
 }
 
-main().catch((err) => {
-  console.log(err);
+main().catch(err => {
+  console.log(err)
 }).then(() => {
-  console.log("done");
-});
+  console.log("done")
+})
