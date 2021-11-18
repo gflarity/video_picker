e67fb5aa08084cae2336a9903a626694d4ade5e0
Geoff Flarity <geoff.flarity@gmail.com>
2021-11-18 18:23:59 -0500

    add support for caching which files have been processed

--- picker.ts
+++ picker.ts
@@ -5,7 +5,7 @@ import { readKeypress, Keypress } from "https://deno.land/x/keypress@0.0.7/mod.t
 import { CHAR_NO_BREAK_SPACE }    from "https://deno.land/std@0.88.0/path/_constants.ts";
 
 // processStdEntry prompts the user to delete or keep the file/entry (or quit altogether).
-async function processStdEntry(entry: WalkEntry, videoPlayer: string, videoPlayerOptions: string | undefined) {
+async function processStdEntry(entry: WalkEntry, videoPlayer: string, videoPlayerOptions: string | undefined, processed: Map<string, boolean>) {
   console.log(`Processing std entry ${entry.path}`)
   // some video players (mpv) need options to be usable by default
   let cmd = [videoPlayer]
@@ -18,49 +18,63 @@ async function processStdEntry(entry: WalkEntry, videoPlayer: string, videoPlaye
   
   FOR: for await (const keypress of readKeypress()) {
     if (keypress.key == "q") {
+      await saveProcessedFile(processed)
       Deno.exit(0)
     } else if (keypress.key == "d") {
       console.log(`deleting ${entry.path}`)
       await Deno.remove(entry.path)
     } else if (keypress.key == "r") {
       console.log(`replaying ${entry.path}`)
-      await processStdEntry(entry, videoPlayer, videoPlayerOptions)
+      await processStdEntry(entry, videoPlayer, videoPlayerOptions, processed)
     } else {
       console.log(`Keeping ${entry.path}`)
+      processed.set(entry.path, true)
+      await saveProcessedFile(processed)
     }
     break FOR
   }
 }
 
-// processSkipEntry let's us treat the first N entries as quickly skippable which is useful for resuming a run for whatever reason.
-async function processSkipEntry(entry: WalkEntry, videoPlayer: string, videoPlayerOptions: string | undefined): Promise<boolean> {
-  console.log(`Processing skip entry ${entry.path}`)
-  console.log("(S)kip/Any Key To Process")
-  let complete = false;
-  for await (const keypress of readKeypress()) {
-      if (keypress.key != "s") {
-        await processStdEntry(entry, videoPlayer, videoPlayerOptions)
-        complete = true
-      }
-      break
-  }
-  return complete
-}
 
 async function main() {
   const videoPlayer = Deno.env.get("VIDEO_PLAYER") || "mplayer";
   const videoPlayerOptions = Deno.env.get("VIDEO_PLAYER_OPTIONS")
   const walker = walk(".", { match: [new RegExp("(mp4|mkv|mov)$")] })
   
-  let skipComplete = false
+  let processed = await loadProcessedFile()
   for await (const entry of walker) {
-    if (!skipComplete) {
-      skipComplete = await processSkipEntry(entry, videoPlayer, videoPlayerOptions)
-    } else {
-      await processStdEntry(entry, videoPlayer, videoPlayerOptions)
+      if (!processed.get(entry.path)) {
+        await processStdEntry(entry, videoPlayer, videoPlayerOptions, processed)
+      }
+  }
+  await saveProcessedFile(processed)
+}
+
+
+async function loadProcessedFile(): Promise<Map<string, boolean>> {
+  const decoder = new TextDecoder('utf-8')
+  try {
+    const data = await Deno.readFile('./.processed.json')
+    let jsonObject = JSON.parse(decoder.decode(data))
+    let processed = new Map<string, boolean>()
+    for (var value in jsonObject) {  
+      processed.set(value,jsonObject[value])  
     }
+    return processed
+  } catch (e) {
+    console.log(e)
+    return new Map<string, boolean>()
   }
 }
+
+async function saveProcessedFile(processed: Map<string, boolean>){
+  try {
+      await Deno.writeTextFile("./.processed.json", JSON.stringify(Object.fromEntries(processed)));
+  } catch(e) {
+      console.log(e);
+    }
+}
+
 
 main().catch(err => {
   console.log(err)
