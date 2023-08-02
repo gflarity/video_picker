adf23c6b4db737afbe18cdd25945398a18f296eb
Geoffrey Flarity <geoff.flarity@gmail.com>
2023-08-02 10:50:55 -0400

    a bit of clean up, main function not needed with deno

--- picker.ts
+++ picker.ts
@@ -1,22 +1,17 @@
-import {
-  walk,
-  WalkEntry,
-  WalkOptions,
-} from "https://deno.land/std@0.170.0/fs/mod.ts";
+import { walk, WalkEntry } from "https://deno.land/std@0.170.0/fs/mod.ts";
 import {
   Keypress,
   readKeypress,
 } from "https://raw.githubusercontent.com/dmitriytat/keypress/6d2ceffe2b4cca0145664b30ffccfbb28f5be737/mod.ts";
 import { CHAR_NO_BREAK_SPACE } from "https://deno.land/std@0.88.0/path/_constants.ts";
-import * as path from "https://deno.land/std@0.170.0/path/mod.ts"
+import * as path from "https://deno.land/std@0.170.0/path/mod.ts";
 
 // processStdEntry prompts the user to delete or keep the file/entry (or quit altogether).
 async function processStdEntry(
   entry: WalkEntry,
   videoPlayer: string,
-  videoPlayerOptions: string | undefined,
+  videoPlayerOptions: string | undefined
 ) {
-
   console.log(`Processing std entry ${entry.path}`);
   // some video players (mpv) need options to be usable by default
   let vpcmd = [videoPlayer];
@@ -31,21 +26,20 @@ async function processStdEntry(
   await p.status();
   console.log("(d)elete/(k)eep/(r)eplay/(q)uit");
 
-  FOR:
-  for await (const keypress of readKeypress()) {
+  FOR: for await (const keypress of readKeypress()) {
     if (keypress.key == "q") {
       Deno.exit(0);
     } else if (keypress.key == "d") {
-      const trashName = "./trash/" + path.basename(entry.path)
+      const trashName = "./trash/" + path.basename(entry.path);
       console.log(`trash: moving ${entry.path} to ${trashName}`);
       await Deno.rename(entry.path, trashName);
     } else if (keypress.key == "r") {
       console.log(`replaying ${entry.path}`);
       await processStdEntry(entry, videoPlayer, videoPlayerOptions);
     } else {
-      const keeperName = "./keepers/" + path.basename(entry.path)
+      const keeperName = "./keepers/" + path.basename(entry.path);
       console.log(`keeper: moving ${entry.path} to ${keeperName}`);
-      Deno.rename(entry.path, keeperName)
+      Deno.rename(entry.path, keeperName);
     }
     break FOR;
   }
@@ -55,7 +49,7 @@ async function processStdEntry(
 async function processSkipEntry(
   entry: WalkEntry,
   videoPlayer: string,
-  videoPlayerOptions: string | undefined,
+  videoPlayerOptions: string | undefined
 ): Promise<boolean> {
   console.log(`Processing skip entry ${entry.path}`);
   console.log("(S)kip/Any Key To Process");
@@ -70,25 +64,20 @@ async function processSkipEntry(
   return complete;
 }
 
-async function main() {
-  const videoPlayer = Deno.env.get("VIDEO_PLAYER") || "mplayer";
-  const videoPlayerOptions = Deno.env.get("VIDEO_PLAYER_OPTIONS");
-  const walker = walk(".", { skip: [new RegExp("keepers"), new RegExp("trash")], match: [new RegExp("(mp4|mkv|mov|iso)$", "i")] });
-
-  await Deno.mkdir("keepers", {recursive:true})
-  await Deno.mkdir("trash", {recursive:true})
-
-  for await (const entry of walker) {
-    // skip directories, it's not an processable entry  
-    if(entry.isDirectory) {
-        continue
-      }
-    await processStdEntry(entry, videoPlayer, videoPlayerOptions);
-  }
-}
-
-main().catch((err) => {
-  console.log(err);
-}).then(() => {
-  console.log("done");
+const videoPlayer = Deno.env.get("VIDEO_PLAYER") || "mplayer";
+const videoPlayerOptions = Deno.env.get("VIDEO_PLAYER_OPTIONS");
+const walker = walk(".", {
+  skip: [new RegExp("keepers"), new RegExp("trash")],
+  match: [new RegExp("(mp4|mkv|mov|iso)$", "i")],
 });
+
+await Deno.mkdir("keepers", { recursive: true });
+await Deno.mkdir("trash", { recursive: true });
+
+for await (const entry of walker) {
+  // skip directories, it's not an processable entry
+  if (entry.isDirectory) {
+    continue;
+  }
+  await processStdEntry(entry, videoPlayer, videoPlayerOptions);
+}
